ce055df2a567b7abe7a97c9a0055a4b88469c9c8
İbrahim Aydın <aydnibrahim22@gmail.com>
2024-01-02 20:17:48 +0300

    Default

--- arayüz.py
+++ arayüz.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 main_page = Tk()
 main_page.title("To Do List")
@@ -17,33 +18,43 @@ title = Label(main_page, text="GÖREVLER", font=("Pt Mono", 20))
 title.place(x=300, y=12)
 
 metin = StringVar(main_page, value="Merhaba")
-text_field = Text(main_page, width=80, height=5)
+text_field = Text(main_page, width=80, height=2)
 text_field.place(x=12, y=300)
 
 
 def complete_task():
-    value = lb.get(lb.curselection()[0])
-    print(value)
+    result = messagebox.askokcancel("?", "Bu görevi tamamlamak istediğine emin misin?", parent=main_page)
+    if result:
+        value = lb.get(lb.curselection()[0])
+        print(value)
+    else:
+        messagebox.showinfo("", "Görev tamamlanmadı.", parent=main_page)
 
 
 def delete_task():
-    value = lb.get(lb.curselection()[0])
-    print(value)
+    result = messagebox.askokcancel("", "Bu görevi silmek istediğine emin misin?", parent=main_page)
+    if result:
+        value = lb.get(lb.curselection()[0])
+        print(value)
+    else:
+        messagebox.showinfo("", "Görev tamamlanmadı.", parent=main_page)
 
+
+def add_task_button():
+    if text_field.get("1.0", "end-1c") == "":
+        messagebox.showerror("Ekleme Hatası", "Boş görev girilemez", parent=main_page)
+    else:
+        lb.insert(END, text_field.get("1.0", "end-1c"))
+        text_field.delete("1.0", "end-1c")
+
+
+add_button = Button(main_page, text="Ekle", command=add_task_button, highlightthickness=0, width=7)
+add_button.place(x=585, y=302)
 
 complete_button = Button(main_page, text="Tamamla", command=complete_task)
 complete_button.place(x=15, y=15)
 
 delete_button = Button(main_page, text="Sil", command=delete_task)
 delete_button.place(x=110, y=15)
-
-
-def add_task_button():
-    lb.insert(END, text_field.get("1.0", "end-1c"))
-    text_field.delete("1.0", "end-1c")
-
-
-add_button = Button(main_page, text="Ekle", command=add_task_button, highlightthickness=0, width=7, height=4)
-add_button.place(x=585, y=302)
 
 main_page.mainloop()
