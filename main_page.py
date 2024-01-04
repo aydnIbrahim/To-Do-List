ea5146a0f967724dfa192e6d7f1efb27918c57d2
İbrahim Aydın <aydnibrahim22@gmail.com>
2024-01-04 19:11:17 +0300

    Default

--- main_page.py
+++ main_page.py
@@ -1,5 +1,8 @@
 import customtkinter as ctk
 from tkinter import *
+from tkinter import messagebox
+
+from PIL import ImageTk, Image
 
 # Ana pencereyi oluştur
 root = ctk.CTk()
@@ -20,11 +23,31 @@ sag_panel.place(x=570, y=15)
 
 
 def tamamla_gorev():
-    print("basıldı")
+    selected_indicies = listbox.curselection()
+    if selected_indicies:
+        selected_item = listbox.get(selected_indicies[0])
+        print(selected_item)
+        r = messagebox.askokcancel(title="", message="Bu görevi tamamlamak istediğine emin misin?")
+        if r:
+            print("Tamam doğru")
+        else:
+            print("Tamam yanlış")
+    else:
+        messagebox.showerror(title='', message="Önce bir görev seçin")
 
 
 def sil_gorev():
-    print('sil görev')
+    selected_indicies = listbox.curselection()
+    if selected_indicies:
+        selected_item = listbox.get(selected_indicies[0])
+        print(selected_item)
+        r = messagebox.askokcancel(title="", message="Bu görevi silmek istediğine emin misin?")
+        if r:
+            print("Sil doğru")
+        else:
+            print("Sil yanlış")
+    else:
+        messagebox.showerror(title='', message="Önce bir görev seçin")
 
 
 # Butonlar için bir çerçeve oluştur
@@ -66,15 +89,15 @@ ekle_buton = ctk.CTkButton(panel, text="Ekle", command=ekle_gorev, fg_color="#00
 ekle_buton.pack(pady=(5, 20))
 
 # Yuvarlak buton için Canvas oluştur
-canvas = Canvas(sag_panel, width=100, height=100, bg="#333333", highlightthickness=0)
-canvas.pack(pady=(5, 20), padx=10)
+canvas_eye = Canvas(sag_panel, width=100, height=100, bg="#333333", highlightthickness=0)
+canvas_eye.pack(pady=(5, 20), padx=10)
 
 # Yuvarlak butonu çiz
-oval_id = canvas.create_oval(10, 10, 90, 90, fill="#2220E2", outline="#0052cc")
+oval_id = canvas_eye.create_oval(10, 10, 90, 90, fill="#2220E2", outline="#0052cc")
 
 # Buton resmini yükle
 buton_resmi = PhotoImage(file="person.png")
-canvas.create_image(50, 50, image=buton_resmi)
+canvas_eye.create_image(50, 50, image=buton_resmi)
 
 # Kullanıcı isim alanı
 metin_alani = ctk.CTkLabel(sag_panel, text="MUAZZEZ ABACI", text_color="#ffffff", corner_radius=10,
@@ -91,10 +114,49 @@ guncelle_eposta_buton = ctk.CTkButton(sag_panel, text="E-Postanı güncelle", fg
                                       text_color="#ffffff", corner_radius=10)
 guncelle_eposta_buton.pack(pady=5, fill='x', padx=10)
 
+password_cerceve = ctk.CTkFrame(sag_panel, fg_color="#333333", corner_radius=10)
+password_cerceve.pack(pady=(40, 5), fill='x', padx=1)
+
 # Şifre güncelleme metin giriş alanı
-guncelle_sifre_entry = ctk.CTkEntry(sag_panel, placeholder_text="Yeni şifren", fg_color="#444444",
-                                    text_color='#fff', corner_radius=10)
-guncelle_sifre_entry.pack(pady=(40, 5), fill='x', padx=10)
+guncelle_sifre_entry = ctk.CTkEntry(password_cerceve, placeholder_text="Yeni şifren", fg_color="#444444",
+                                    text_color='#fff', corner_radius=10, width=150, show='*')
+guncelle_sifre_entry.pack(side=LEFT, padx=2)
+
+# Göz ikonunu yükleyin ve bir Tkinter PhotoImage'a dönüştürün
+eye_icon = Image.open("eye.png")
+eye_photo_image = ImageTk.PhotoImage(eye_icon)
+
+eye_slash_icon = Image.open("eye.slash.png")
+eye_slash_photo_image = ImageTk.PhotoImage(eye_slash_icon)
+
+# Canvas oluşturun ve arka planını şeffaf yapın
+canvas_eye = Canvas(password_cerceve, width=40, height=40, bg='#333333', highlightthickness=0)
+canvas_eye.pack(side=LEFT, padx=1)
+
+# Canvas üzerine göz ikonunu ekleyin
+eye_button = canvas_eye.create_image(10, 10, anchor='nw', image=eye_photo_image)
+canvas_eye.itemconfigure(eye_button, state='normal')
+
+# Canvas üzerine göz çizgi ikonunu ekleyin
+eye_slash_button = canvas_eye.create_image(10, 10, anchor='nw', image=eye_slash_photo_image)
+canvas_eye.itemconfigure(eye_slash_button, state='hidden')
+
+
+# Göz ikonuna tıklama olayı ekleyin
+def on_eye_click(event):
+    guncelle_sifre_entry.configure(show='')
+    canvas_eye.itemconfigure(eye_button, state='hidden')
+    canvas_eye.itemconfigure(eye_slash_button, state='normal')
+
+
+def on_eye_slash_click(event):
+    guncelle_sifre_entry.configure(show='*')
+    canvas_eye.itemconfigure(eye_button, state='normal')
+    canvas_eye.itemconfigure(eye_slash_button, state='hidden')
+
+
+canvas_eye.tag_bind(eye_button, '<Button-1>', on_eye_click)
+canvas_eye.tag_bind(eye_slash_button, '<Button-1>', on_eye_slash_click)
 
 # 'Şifreni güncelle' butonunu oluştur
 guncelle_sifre_buton = ctk.CTkButton(sag_panel, text="Şifreni güncelle", fg_color="#0052cc", hover_color="#003380",
